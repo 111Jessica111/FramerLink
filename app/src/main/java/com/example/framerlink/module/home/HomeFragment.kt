00ecde88ffb0de9e333e394eb3ccf7f46293c0f4
111Jessica111 <165466969+111Jessica111@users.noreply.github.com>
2025-07-25 16:48:41 +0800

    [feat]首页UI Dailog 逻辑实现

--- app/src/main/java/com/example/framerlink/module/home/HomeFragment.kt
+++ app/src/main/java/com/example/framerlink/module/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.framerlink.module.home
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +10,8 @@ import android.view.ViewGroup
 import com.example.framerlink.base.BaseFragment
 import com.example.framerlink.constant.PageName
 import com.example.framerlink.databinding.FragmentHomeBinding
+import com.example.framerlink.databinding.LeadDialogBinding
+import com.tencent.mmkv.MMKV
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
@@ -16,7 +21,29 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //首次进入显示LeadDailog
+        val mmkv = MMKV.defaultMMKV()
+        val isFirst = mmkv.decodeBool("is_first_enter", true)
+        if (isFirst){
+            setLeadDialog()
+            mmkv.encode("is_first_enter", false)
+        }
+
         setFunctionClick(viewBinding)
+    }
+
+    private fun setLeadDialog() {
+        val dialog = Dialog(requireContext())
+        val binding = LeadDialogBinding.inflate(LayoutInflater.from(context))
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setContentView(binding.root)
+        dialog.setCancelable(false)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        binding.btnCancel.setOnClickListener { dialog.dismiss() }
+        dialog.show()
     }
 
     private fun setFunctionClick(binding: FragmentHomeBinding) {
